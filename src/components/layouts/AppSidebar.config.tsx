2b702c4ea65e3e2fc608f4939dab4084be6c65d3
Khatejang <aguarinmarjorie@gmail.com>
2026-04-15 10:46:18 +0800

    :  modify - make the sidebar dynamic based on the role of the user

--- src/components/layouts/AppSidebar.config.tsx
+++ src/components/layouts/AppSidebar.config.tsx
@@ -5,10 +5,12 @@ import {
   RequestQuote,
   ManageAccounts,
   FolderManaged,
+  ListAlt
 } from "@nine-thirty-five/material-symbols-react/rounded";
 import type { NavItem } from "./AppSidebarUtils";
+import { ROLES, type Role } from "@/types/roles";
 
-export const NAV_ITEMS: NavItem[] = [
+export const CLIENT_ITEMS: NavItem[] = [
   {
     id: "dashboard",
     icon: <Dashboard width="2rem" height="2rem" />,
@@ -75,6 +77,140 @@ export const NAV_ITEMS: NavItem[] = [
     path: "/tools",
   },
 ];
+
+export const AS_ITEMS: NavItem[] = [
+  {
+    id: "dashboard",
+    icon: <Dashboard width="2rem" height="2rem" />,
+    label: "Dashboard",
+    path: "/",
+  },
+  {
+    id: "leads",
+    icon: <DiversityTwo width="2rem" height="2rem" />,
+    label: "Leads",
+    subItems: [
+      { label: "Queries", path: "/leads/queries" },
+      { label: "New", path: "/leads/new" },
+      { label: "Replied", path: "/leads/replied" },
+    ],
+  },
+  {
+    id: "shipments",
+    icon: <BoxIcon width="2rem" height="2rem" />,
+    label: "Shipments",
+    subItems: [
+      {
+        label: "Logistics",
+        path: "/shipments/logistics",
+        subItems: [
+          { label: "Ongoing", path: "/shipments/logistics/ongoing" },
+          { label: "Delivered", path: "/shipments/logistics/delivered" },
+        ],
+      },
+      {
+        label: "Regulatory",
+        path: "/shipments/regulatory",
+        subItems: [
+          { label: "Permits", path: "/shipments/regulatory/permits" },
+          { label: "Licenses", path: "/shipments/regulatory/licenses" },
+        ],
+      },
+    ],
+  },
+  {
+    id: "quotations",
+    icon: <RequestQuote width="2rem" height="2rem" />,
+    label: "Quotations",
+    subItems: [
+      { label: "Requests", path: "/quotations/requested" },
+      { label: "Responded", path: "/quotations/responded" },
+      { label: "Accepted", path: "/quotations/accepted" },
+      { label: "Discarded", path: "/quotations/discarded" },
+    ],
+  },
+  {
+    id: "accounts",
+    icon: <ManageAccounts width="2rem" height="2rem" />,
+    label: "Accounts",
+    subItems: [
+      { label: "Clients", path: "/accounts/clients" },
+      { label: "Employees", path: "/accounts/employees" },
+    ],
+  },
+  {
+    id: "tools",
+    icon: <FolderManaged width="2rem" height="2rem" />,
+    label: "Tools",
+    path: "/tools",
+  },
+];
+
+export const OPS_ITEMS: NavItem[] = [
+  {
+    id: "dashboard",
+    icon: <Dashboard width="2rem" height="2rem" />,
+    label: "Dashboard",
+    path: "/",
+  },
+   {
+    id: "jobOrders",
+    icon: <ListAlt width="2rem" height="2rem" />,
+    label: "Job Orders",
+    path: "/job-orders",
+  },
+  {
+    id: "shipments",
+    icon: <BoxIcon width="2rem" height="2rem" />,
+    label: "Shipments",
+    subItems: [
+      {
+        label: "Logistics",
+        path: "/shipments/logistics",
+        subItems: [
+          { label: "Ongoing", path: "/shipments/logistics/ongoing" },
+          { label: "Delivered", path: "/shipments/logistics/delivered" },
+        ],
+      },
+      {
+        label: "Regulatory",
+        path: "/shipments/regulatory",
+        subItems: [
+          { label: "Permits", path: "/shipments/regulatory/permits" },
+          { label: "Licenses", path: "/shipments/regulatory/licenses" },
+        ],
+      },
+    ],
+  },
+  {
+    id: "quotations",
+    icon: <RequestQuote width="2rem" height="2rem" />,
+    label: "Quotations",
+    subItems: [
+      { label: "Requests", path: "/quotations/requested" },
+      { label: "Responded", path: "/quotations/responded" },
+      { label: "Accepted", path: "/quotations/accepted" },
+      { label: "Discarded", path: "/quotations/discarded" },
+    ],
+  },
+
+];
+
+export function getSidebarItemsForRole(role?: Role): NavItem[] {
+  if (role === ROLES.CLIENT) {
+    return CLIENT_ITEMS;
+  }
+
+  if (role === ROLES.LEAD_ACCOUNT_SPECIALIST) {
+    return AS_ITEMS;
+  }
+
+  if (role === ROLES.OPERATIONS) {
+    return OPS_ITEMS;
+  }
+
+  return AS_ITEMS;
+}
 
 export const BTN_HEIGHT_REM = 5;
 export const PILL_HEIGHT_REM = 3.5;
